4e9336f056dbfb62d73d9b63b681bc7491d097f1
the_Globe <keshavkr513@gmail.com>
2026-09-21 11:55:01 +0530

    Update greeting message to V4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Automated CI/CD! V3"
+    return "Hello from Automated CI/CD! V4"
 
 @app.route("/health")
 def health():
